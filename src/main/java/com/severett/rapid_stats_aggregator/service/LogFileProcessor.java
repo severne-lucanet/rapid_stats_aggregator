b745c54418e4975013ab635db5d58222a61905aa
severn-everett <severn.everett@gmail.com>
2017-09-19 15:34:15 +0200

    Added file output for logging processor and unit test for said action

--- src/main/java/com/severett/rapid_stats_aggregator/service/LogFileProcessor.java
+++ src/main/java/com/severett/rapid_stats_aggregator/service/LogFileProcessor.java
@@ -1,13 +1,21 @@
 package com.severett.rapid_stats_aggregator.service;
 
 import com.severett.rapid_stats_aggregator.dto.InputDTO;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.time.Clock;
+import java.time.Instant;
 import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
 import org.apache.commons.compress.archivers.zip.ZipFile;
 import org.apache.commons.compress.utils.SeekableInMemoryByteChannel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import reactor.bus.Event;
 import reactor.fn.Consumer;
@@ -17,6 +25,9 @@ public class LogFileProcessor implements Consumer<Event<InputDTO<byte[]>>> {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(LogFileProcessor.class);
     
+    @Value("${com.severett.rapid_stats_aggregator.logdirectory}")
+    private String logDirectory;
+    
     @Override
     public void accept(Event<InputDTO<byte[]>> event) {
         InputDTO<byte[]> inputDTO = event.getData();
@@ -24,7 +35,12 @@ public class LogFileProcessor implements Consumer<Event<InputDTO<byte[]>>> {
         try (SeekableInMemoryByteChannel inMemoryByteChannel = new SeekableInMemoryByteChannel(inputDTO.getPayload())) {
             try (ZipFile zipFile = new ZipFile(inMemoryByteChannel)) {
                 ZipArchiveEntry archiveEntry = zipFile.getEntries().nextElement();
-                InputStream inputStream = zipFile.getInputStream(archiveEntry);
+                try (InputStream inputStream = zipFile.getInputStream(archiveEntry)) {
+                    Instant now = Clock.systemUTC().instant();
+                    Path fileOutputPath = Paths.get(logDirectory, String.format("%s_%d.txt", inputDTO.getComputerUuid(), now.toEpochMilli()));
+                    LOGGER.debug("Writing log file to {}", fileOutputPath);
+                    Files.copy(inputStream, fileOutputPath, StandardCopyOption.REPLACE_EXISTING);
+                }
             }
         } catch (IOException ioe) {
             LOGGER.error("Error parsing log data from {}: {}", inputDTO.getComputerUuid(), ioe.getMessage());
---
+++ src/test/java/com/severett/rapid_stats_aggregator/service/LogFileProcessorTest.java
@@ -0,0 +1,45 @@
+package com.severett.rapid_stats_aggregator.service;
+
+import com.severett.rapid_stats_aggregator.dto.InputDTO;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import org.apache.commons.compress.utils.IOUtils;
+import static org.hamcrest.Matchers.*;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.springframework.test.util.ReflectionTestUtils;
+import reactor.bus.Event;
+
+public class LogFileProcessorTest {
+
+    private LogFileProcessor logFileProcessor;
+    
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+    
+    private File tempDir;
+    private static final File GOOD_RESOURCES_DIRECTORY = new File("src/test/resources/good_logs");
+    
+    @Before
+    public void setup() throws IOException {
+        tempDir = folder.newFolder("tempDir");
+        tempDir.deleteOnExit();
+        logFileProcessor = new LogFileProcessor();
+        ReflectionTestUtils.setField(logFileProcessor, "logDirectory", tempDir.getPath());
+    }
+    
+    @Test
+    public void creatLogFileTest() throws IOException {
+        File testLogFile = new File(GOOD_RESOURCES_DIRECTORY.getAbsoluteFile(), "lorem_ipsum.zip");
+        try (InputStream inputStream = new FileInputStream(testLogFile)) {
+            logFileProcessor.accept(Event.wrap(new InputDTO<byte[]>("abc123", IOUtils.toByteArray(inputStream))));
+            Assert.assertThat(tempDir.listFiles().length, is(1));
+        }
+    }
+    
+}
